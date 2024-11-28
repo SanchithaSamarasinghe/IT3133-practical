205bb370f6e302697663cb9c63f702911ebc899e
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2024-11-28 17:36:31 +0530

    Using React Router DOM

---
+++ day8/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Link,NavLink } from 'react-router-dom';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Dashbord from './Components/Dashbord';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+
+        <Routes>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/contact' element={<ContactUs/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/dash' element={<Dashbord/>}/>
+        </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ day8/src/Components/About.js
@@ -0,0 +1,6 @@
+export default function About(){
+    return(
+        <div>Welcome to About page</div>
+    );
+
+}
